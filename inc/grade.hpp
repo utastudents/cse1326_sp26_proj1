d25c3eacd2a884c5217b22cf8c2b23143a7bb3f4
nmc6925 <johndoe@example.com>
2026-02-17 09:56:31 -0600

    Started creation of base class - Noah Chiarelli

--- inc/grade.hpp
+++ inc/grade.hpp
@@ -3,3 +3,27 @@ struct grade
 {
 	grade();
 };
+
+// Scoped Enumeration that could be used to discern the indecies in
+// the grades array?? 
+enum class LetterGrade {A, B, C, D, F, I, P, Q, W, Z, R};
+enum class Semester {FALL, WINTER, SPRING, SUMMER};
+
+// Created a rough outline for the class to hold data, probably incorrect - [Noah Chiarelli - February 17th 9:54AM]
+class baseClass{
+
+	char* subjectID;
+	int courseNumber;
+	char* courseTitle;
+	int sectionNumber;
+	int grades[11]; // Index 0 = # of A's index 1 = # of B's etc...
+	
+	int gradesCount;
+	int year;
+
+	// Maybe have linked list of instructors??
+
+	float courseGPA; // Include method to calculate GPA? Unless it's already included in JSON
+	float drop_percent; 
+
+};
